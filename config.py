2ce3b6e43e60456ce5ec4257c885016ecc3c476e
hone1er <josephmvillavicencio@gmail.com>
2019-10-29 13:59:46 -0700

    fix untracked files

--- config.py
+++
@@ -1,2 +0,0 @@
-mongop = 'mongodb+srv://hone1er:Penalbaby1@incometracker-blo7g.azure.mongodb.net/test?retryWrites=true&w=majority'
-SECRET_KEY = 'F\xd6\xde\xfea\xc4\x85_a\xa8c\xb1mL\xd5\xc0O\xffD\xb0|<]\xaa!'
